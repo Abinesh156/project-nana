76c7d3222459fbc9c12ce9289f90ee80694d2557
abinesh <s.abinesh102@h.com>
2024-12-15 14:33:33 +0530

    hack file from mobile

---
+++ hack.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "type a name "
+read name 
+clear
+sleep 3 
+echo "welcome to the realworld $name"
+sleep 3
+cowsay your a dick $name
+sleep 4
+
+
+cowsay time to hack your mobile $name
+figlet umbu $name
+
+sleep 5
+cmatrix
+
+
